acad296fb4456ab923fa70f91436bfc2e298bd9c
Emile Salem <emile.salem@Salems-lot.local>
2018-10-04 16:59:38 -0400

    continuing

--- consul/consul.go
+++ consul/consul.go
@@ -1,90 +1,74 @@
+/*
+ */
 package consul
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/apex/log"
 	consul "github.com/hashicorp/consul/api"
 	consulWatch "github.com/hashicorp/consul/watch"
 )
-
-const ServiceDiscoveryRefreshDuration = 10 * time.Second
 
 type ServiceInfo struct {
 	Name string
 	URL  string
 }
 
-/*
-see:
-https://www.consul.io/docs/agent/watches.html
-https://github.com/hashicorp/consul/blob/master/watch/plan.go
-https://github.com/hashicorp/consul/blob/master/watch/watch.go#L116
-https://gowalker.org/github.com/hashicorp/consul/watch#WatcherFunc
-https://godoc.org/github.com/hashicorp/consul/watch#Plan
-https://godoc.org/github.com/hashicorp/consul/api#CatalogService
+func watchService(serviceInfo *ServiceInfo, client *consul.Client) error {
+	plan, err := createServiceWatchPlan(serviceInfo)
+	if err != nil {
+		return err
+	}
+	plan.Handler = func(someInt uint64, result interface{}) {
+		var results []*consul.ServiceEntry = result.([]*consul.ServiceEntry)
+		selectNodeAddress(serviceInfo, results)
+	}
+	err = plan.RunWithClientAndLogger(client, nil)
+	if err != nil {
+		log.WithError(err).Error("error during service watch")
+		return err
+	}
+	return nil
+}
 
-*/
-func watchService(serviceName string, client *consul.Client) {
+func createServiceWatchPlan(serviceInfo *ServiceInfo) (*consulWatch.Plan, error) {
 	watchQuery := make(map[string]interface{})
 	watchQuery["type"] = "service"
-	watchQuery["service"] = serviceName
+	watchQuery["service"] = serviceInfo.Name
 	plan, err := consulWatch.Parse(watchQuery)
 	if err != nil {
 		log.WithError(err).Error("cant parse watch query parameters")
-	} else {
-		log.Info(fmt.Sprintf("plan info! %v", plan))
+		return nil, err
 	}
-	plan.Handler = func (someInt uint64, result consul.CatalogService) {
-		log.Info(fmt.Sprintf("%v", result.Address))
-	}
-	plan.RunWithClientAndLogger(client, nil)
+	return plan, nil
 }
 
-func findService(catalog *consul.Catalog, serviceInfo *ServiceInfo) {
-	services, _, err := catalog.Service(serviceInfo.Name, "", nil)
-
-	if err != nil {
-		log.WithError(err).Error("can't get services from catalog")
-		return
-	}
-
-	if len(services) <= 0 {
-		log.WithField("name", serviceInfo.Name).Error("service not found")
-		return
-	}
-
-	for _, s := range services {
-		if url := fmt.Sprintf("http://%v:%v", s.ServiceAddress, s.ServicePort); url != serviceInfo.URL {
+func selectNodeAddress(serviceInfo *ServiceInfo, nodes []*consul.ServiceEntry) {
+	for _, n := range nodes {
+		serviceAddress := n.Service.Address
+		if len(serviceAddress) == 0 {
+			serviceAddress = n.Node.Address
+		}
+		if serviceAddress != serviceInfo.URL {
 			log.WithFields(log.Fields{
 				"name": serviceInfo.Name,
-				"url":  url,
+				"url":  serviceAddress,
 			}).Info("updating node")
-			serviceInfo.URL = url
+			serviceInfo.URL = serviceAddress
 			return
 		}
 	}
 }
 
-func discover(serviceInfo *ServiceInfo) {
+func WatchServiceURL(serviceName string) (*string, error) {
+	serviceInfo := &ServiceInfo{serviceName, ""}
 	client, err := consul.NewClient(&consul.Config{Address: ConsulAddress})
 	if err != nil {
 		log.WithError(err).Error("can't connect to consul")
-	} else {
-		findService(client.Catalog(), serviceInfo)
-		watchService(serviceInfo.Name, client)
+		return nil, err
 	}
-}
-
-func WatchService(serviceName string) *string {
-	serviceInfo := &ServiceInfo{serviceName, ""}
-	discover(serviceInfo) // invoke first tick
-	// ticker := time.NewTicker(ServiceDiscoveryRefreshDuration)
-	// go func() {
-	// 	for range ticker.C {
-	// 		discover(serviceInfo)
-	// 	}
-	// }()
-	return &serviceInfo.URL
+	err = watchService(serviceInfo, client)
+	if err != nil {
+		return nil, err
+	}
+	return &serviceInfo.URL, nil
 }
--- consul/consul_test.go
+++ consul/consul_test.go
@@ -1,16 +1,21 @@
 package consul
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/apex/log"
 )
 
 func TestWatchServiceTest(t *testing.T) {
-	if url := WatchService("api-core"); url == nil {
+	if url, err := WatchServiceURL("api-core"); err != nil || url == nil {
 		new(testing.T).Error(
 			"For", "watch service test",
 			"expected", "something",
 			"got", "nothing",
 		)
+	} else {
+		log.Info(fmt.Sprintf("test success, got URL: %s", *url))
 	}
-	
+
 }
