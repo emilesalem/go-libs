88c4b1649b62b91f1a59987485b01a3d88a33721
emile <emile.salem@workjam.com>
2018-10-05 22:34:34 -0400

    ongoing

--- consul/consul.go
+++ consul/consul.go
@@ -1,5 +1,4 @@
-/*
- */
+// Package consul provides the means to get the changing value of a service URL
 package consul
 
 import (
@@ -10,59 +9,47 @@ import (
 	"time"
 
 	"github.com/apex/log"
-	consul "github.com/hashicorp/consul/api"
-	consulWatch "github.com/hashicorp/consul/watch"
+	consulapi "github.com/hashicorp/consul/api"
+	consulwatch "github.com/hashicorp/consul/watch"
 )
 
+//ServiceInfo holds the name and the URL of a service
 type ServiceInfo struct {
 	Name string
 	URL  string
 }
 
-func watchService(serviceInfo *ServiceInfo, client *consul.Client) {
-	plan, err := createServiceWatchPlan(serviceInfo)
+//WatchServiceURL returns a pointer to a ServiceInfo holding the current URL of the service
+//this URL value will get updated as the service nodes change
+func WatchServiceURL(serviceName string) (*ServiceInfo, error) {
+	serviceInfo := &ServiceInfo{serviceName, ""}
+	client, err := consulapi.NewClient(&consulapi.Config{Address: ConsulAddress})
 	if err != nil {
-		log.WithError(err).Error("error creating service watch")
-		return
+		log.WithError(err).Error("can't connect to consul")
+		return nil, err
 	}
-	plan.Handler = func(someInt uint64, result interface{}) {
-		results := result.([]*consul.ServiceEntry)
-		selectServiceEntryNodeAddress(results)
-		log.Info(fmt.Sprintf("updating service address: %s", serviceInfo.URL))
+	if serviceInfo.URL, err = fetchCurrentServiceURL(serviceInfo.Name, *client); err != nil {
+		return nil, err
 	}
-	err = plan.RunWithClientAndLogger(client, stdLog.New(os.Stderr, "", 1))
-	if err != nil {
-		log.WithError(err).Error("error starting service watch")
-	}
+	go watchService(serviceInfo, *client)
+	return serviceInfo, nil
 }
 
-func fetchInitialServiceURL(serviceName string, catalog *consul.Catalog) (string, error) {
-	nodes, _, err := catalog.Service(serviceName, "", nil)
+func fetchCurrentServiceURL(serviceName string, client consulapi.Client) (string, error) {
+	nodes, _, err := client.Health().Service(serviceName, "", true, &consulapi.QueryOptions{AllowStale: false})
 	if err != nil {
-		log.WithError(err).Error("can't get services from catalog")
+		log.WithError(err).Error("can't get services from consul")
 		return "", err
 	}
 	if len(nodes) <= 0 {
-		log.WithField("name", serviceName).Error("service not found")
+		log.WithField("name", serviceName).Error(fmt.Sprintf("%s service not found", serviceName))
 		return "", err
 	}
-	return selectCatalogEntryNodeAddress(nodes), nil
+	return selectServiceEntryAddress(nodes), nil
 }
 
-func createServiceWatchPlan(serviceInfo *ServiceInfo) (*consulWatch.Plan, error) {
-	watchQuery := make(map[string]interface{})
-	watchQuery["type"] = "service"
-	watchQuery["service"] = serviceInfo.Name
-	plan, err := consulWatch.Parse(watchQuery)
-	if err != nil {
-		log.WithError(err).Error("cant parse watch query parameters")
-		return nil, err
-	}
-	return plan, nil
-}
-
-func selectServiceEntryNodeAddress(nodes []*consul.ServiceEntry) string {
-	var result string = ""
+func selectServiceEntryAddress(nodes []*consulapi.ServiceEntry) string {
+	var result string
 	if len(nodes) > 0 {
 		r1 := rand.New(rand.NewSource(time.Now().UnixNano()))
 		n := nodes[r1.Intn(len(nodes))]
@@ -75,30 +62,31 @@ func selectServiceEntryNodeAddress(nodes []*consul.ServiceEntry) string {
 	return result
 }
 
-func selectCatalogEntryNodeAddress(nodes []*consul.CatalogService) string {
-	var result string = ""
-	if len(nodes) > 0 {
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		n := nodes[r.Intn(len(nodes))]
-		serviceAddress := n.ServiceAddress
-		if len(serviceAddress) == 0 {
-			serviceAddress = n.Address
-		}
-		result = fmt.Sprintf("%s:%v", serviceAddress, n.ServicePort)
+func watchService(serviceInfo *ServiceInfo, client consulapi.Client) {
+	plan, err := createServiceWatchPlan(serviceInfo.Name)
+	if err != nil {
+		log.WithError(err).Error("error creating service watch plan")
+		return
 	}
-	return result
+	plan.Handler = func(someInt uint64, result interface{}) {
+		serviceInfo.URL = selectServiceEntryAddress(result.([]*consulApi.ServiceEntry))
+		log.Info(fmt.Sprintf("updating %s service, new URL: %s", serviceInfo.Name, serviceInfo.URL))
+	}
+	err = plan.RunWithClientAndLogger(&client, stdLog.New(os.Stderr, "", 1))
+	if err != nil {
+		log.WithError(err).Error("error starting service watch")
+	}
 }
 
-func WatchServiceURL(serviceName string) (*ServiceInfo, error) {
-	serviceInfo := &ServiceInfo{serviceName, ""}
-	client, err := consul.NewClient(&consul.Config{Address: ConsulAddress})
+func createServiceWatchPlan(serviceName string) (*consulwatch.Plan, error) {
+	watchQuery := make(map[string]interface{})
+	watchQuery["type"] = "service"
+	watchQuery["service"] = serviceName
+	watchQuery["passingonly"] = true
+	plan, err := consulwatch.Parse(watchQuery)
 	if err != nil {
-		log.WithError(err).Error("can't connect to consul")
+		log.WithError(err).Error("cant parse watch query parameters")
 		return nil, err
 	}
-	if serviceInfo.URL, err = fetchInitialServiceURL(serviceInfo.Name, client.Catalog()); err != nil {
-		return nil, err
-	}
-	go watchService(serviceInfo, client)
-	return serviceInfo, nil
+	return plan, nil
 }
